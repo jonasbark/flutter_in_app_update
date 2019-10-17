d2d24113e74e18dff575d75af5a95b2c21c5d0a4
Jonas Bark <jonas.bark@ffuf.de>
2019-10-17 15:08:47 +0200

    change authors in pubspec

--- test/in_app_update_test.dart
+++ test/in_app_update_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:in_app_update/in_app_update.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('in_app_update');
